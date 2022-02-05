8485cb002a65c84b1d714255887776d08e71041c
Aditya <62930521+adityar224@users.noreply.github.com>
2022-02-05 09:05:20 +0530

    ✨ updated name and icons
    ✨ updated name and icons

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -27,7 +27,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.CreateAndShow(L"random_face_generator", origin, size)) {
+  if (!window.CreateAndShow(L"Random Face Generator", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
